bc44cccb6699c772460b6c11e571d7f2de691cdf
maxsean <xsma1990@gmail.com>
2017-11-01 16:02:30 -0400

    Added homepage styling

--- app/javascript/react/src/App.js
+++ app/javascript/react/src/App.js
@@ -11,6 +11,11 @@ const App = (props) => {
         <Route path='/' component={NavBar}>
           <IndexRoute component={WelcomeContainer} />
           <Route path='/signup' component={SignUpFormContainer}/>
+          <Route path='/about' component={AboutContainer}/>
+          <Route path='/help' component={HelpContainer}/>
+          <Route path='/news' component={NewsContainer}/>
+          <Route path='/demo' component={DemoContainer}/>
+          <Route path='/contact' component={ContactContainer}/>
         </Route>
       </Router>
     </div>
--- app/javascript/react/src/components/NavBar.js
+++ app/javascript/react/src/components/NavBar.js
@@ -6,7 +6,6 @@ const NavBar = props => {
   return(
     <div>
       <div className="navBar">
-        <BackButton className="backButton"/>
         <Link to='/' className="home"> YANA </Link>
       </div>
         {props.children}
